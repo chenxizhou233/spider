0615fc4536efb1daf071f0a8627592a7689c9238
Xizhou Chen <chenxizhou2005@outlook.com>
2026-05-20 10:38:10 +0200

    Finished the thread version of the craw.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
+mod download_thread;
 mod downloader;
 mod task;
 
-use calamine::{Reader, open_workbook_auto};
-
-use crate::task::CrawlTask;
+use crate::download_thread::download_thread;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut workbook =
-        open_workbook_auto("/Users/chenxizhou/CS/2026s-OS/AsyncOS/spider/高校名称和官方网站.xlsx")?;
-    let range = workbook.worksheet_range_at(0).unwrap()?;
-    for row in range.rows().skip(1) {
-        let uni = row.get(0).map(|c| c.to_string()).unwrap_or_default();
-        let url = row.get(1).map(|c| c.to_string()).unwrap_or_default();
-        let tmp = CrawlTask { uni, url };
-        tmp.run();
-    }
-    Ok(())
+    download_thread()
 }
