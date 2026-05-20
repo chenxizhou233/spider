7b539c8076337e7f77b65d3844ce6c6d9be95400
Xizhou Chen <chenxizhou2005@outlook.com>
2026-05-20 10:45:51 +0200

    Fixed some style issue.

--- src/download_thread.rs
+++ src/download_thread.rs
@@ -1,20 +1,11 @@
-use crate::task::CrawlTask;
-use calamine::{Reader, open_workbook_auto};
-use std::{thread::spawn, vec};
+use crate::task::creat_task_queue;
+use std::thread::spawn;
 
 pub fn download_thread() -> Result<(), Box<dyn std::error::Error>> {
-    let mut tasks: Vec<CrawlTask> = vec![];
-    let mut workbook =
-        open_workbook_auto("/Users/chenxizhou/CS/2026s-OS/AsyncOS/spider/高校名称和官方网站.xlsx")?;
-    let range = workbook.worksheet_range_at(0).unwrap()?;
-    for row in range.rows().skip(1) {
-        let uni = row.get(0).map(|c| c.to_string()).unwrap_or_default();
-        let url = row.get(1).map(|c| c.to_string()).unwrap_or_default();
-        tasks.push(CrawlTask { uni, url });
-    }
+    let tasks = creat_task_queue().unwrap();
     let handles: Vec<_> = tasks.into_iter().map(|x| spawn(move || x.run())).collect();
     for handle in handles {
-        handle.join().unwrap();
+        let _ = handle.join().unwrap();
     }
     Ok(())
 }
--- src/task.rs
+++ src/task.rs
@@ -1,4 +1,5 @@
 use crate::downloader::download_sync;
+use calamine::{Reader, open_workbook_auto};
 use std::path::PathBuf;
 
 #[derive(Debug, Clone)]
@@ -14,4 +15,16 @@ impl CrawlTask {
     pub fn run(&self) -> anyhow::Result<()> {
         download_sync(&self.url, self.output_path())
     }
+}
+pub fn creat_task_queue() -> anyhow::Result<Vec<CrawlTask>> {
+    let mut tasks: Vec<CrawlTask> = vec![];
+    let mut workbook =
+        open_workbook_auto("/Users/chenxizhou/CS/2026s-OS/AsyncOS/spider/高校名称和官方网站.xlsx")?;
+    let range = workbook.worksheet_range_at(0).unwrap()?;
+    for row in range.rows().skip(1) {
+        let uni = row.first().map(|c| c.to_string()).unwrap_or_default();
+        let url = row.get(1).map(|c| c.to_string()).unwrap_or_default();
+        tasks.push(CrawlTask { uni, url });
+    }
+    Ok(tasks)
 }
