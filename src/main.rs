0d282b378691d762c9eea4ce3b9791cf388be27e
Xizhou Chen <chenxizhou2005@outlook.com>
2026-05-21 10:42:22 +0200

    Deleted the file that don't need. Added the structure of the coroutine version.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-mod download_process;
 mod download_thread;
 mod downloader;
 mod task;
 
-use crate::{download_process::download_process, download_thread::download_thread};
+use crate::download_thread::download_thread;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
